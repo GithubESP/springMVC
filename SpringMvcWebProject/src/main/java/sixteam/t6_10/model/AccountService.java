6a05d87fffd6ce26a2cc2890532dbf48b5f534f0
Liu-YiTing <winterspirit000@gmail.com>
2023-01-05 10:43:14 +0800

    管理者Service修改完成

---
+++ SpringMvcWebProject/src/main/java/sixteam/t6_10/model/AccountService.java
@@ -0,0 +1,17 @@
+package sixteam.t6_10.model;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+@Service
+@Transactional
+public class AccountService {
+	
+	@Autowired
+	private AccountManagerDao aDao;
+	
+	public boolean checkLogin(AccountManager accountManager) {
+		return aDao.checkLogin(accountManager);
+	}
+}
